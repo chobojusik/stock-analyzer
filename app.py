acdb409e0c9fa244ad042b0000007ff787f3985a
초보주식 <frog88v@gmail.com>
2026-05-18 21:20:19 +0900

    add korean search

--- app.py
+++ app.py
@@ -20,4 +20,21 @@ try:
 
 except:
     st.error("종목코드를 확인하세요.")
-    
+    user_input = st.text_input("종목명 또는 종목코드 입력", "삼성전자")
+
+listing = fdr.StockListing('KRX')
+
+# 숫자면 종목코드로 처리
+if user_input.isdigit():
+    code = user_input
+
+# 문자면 종목명 검색
+else:
+    result = listing[listing['Name'] == user_input]
+
+    if len(result) > 0:
+        code = result.iloc[0]['Code']
+    else:
+        st.error("종목명을 찾을 수 없습니다.")
+        st.stop()
+        
