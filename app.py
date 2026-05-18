07ca37cd2bfdcb9681f58e99f1135fe0b5931bb7
초보주식 <frog88v@gmail.com>
2026-05-18 21:37:26 +0900

    fix indent

--- app.py
+++ app.py
@@ -26,12 +26,12 @@ else:
         st.stop()
 
 df = fdr.DataReader(code)
-
 st.write(df.tail())
-
 current_price = df['Close'].iloc[-1]
-
 st.metric("현재가", f"{current_price:,}원")
+
+
+
 
 
 
@@ -39,7 +39,7 @@ st.metric("현재가", f"{current_price:,}원")
    
 
   
-    user_input = st.text_input("종목명 또는 종목코드 입력", "삼성전자")
+user_input = st.text_input("종목명 또는 종목코드 입력", "삼성전자")
 
 listing = fdr.StockListing('KRX')
 
@@ -56,7 +56,7 @@ else:
     else:
         st.error("종목명을 찾을 수 없습니다.")
         st.stop()
-      user_input = st.text_input("종목코드 또는 종목명 입력", "005930")
+    user_input = st.text_input("종목코드 또는 종목명 입력", "005930")
 
 listing = fdr.StockListing('KRX')
 
