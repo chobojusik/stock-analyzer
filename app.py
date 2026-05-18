d59135587e6fb23550839f415204db2675b54ab0
초보주식 <frog88v@gmail.com>
2026-05-18 21:32:04 +0900

    fix app

--- app.py
+++ app.py
@@ -7,19 +7,38 @@ import FinanceDataReader as fdr
 
 st.title("국내 주식 분석기")
 
-code = st.text_input("종목코드 입력", "005930")
+user_input = st.text_input("종목명 또는 종목코드 입력", "삼성전자")
 
-try:
-    df = fdr.DataReader(code)
+listing = fdr.StockListing('KRX')
 
-    st.write(df.tail())
+# 숫자면 종목코드 처리
+if user_input.isdigit():
+    code = user_input
 
-    current_price = df['Close'].iloc[-1]
+# 문자면 종목명 검색
+else:
+    result = listing[listing['Name'] == user_input]
 
-    st.metric("현재가", f"{current_price:,.0f}원")
+    if len(result) > 0:
+        code = result.iloc[0]['Code']
+    else:
+        st.error("종목명을 찾을 수 없습니다.")
+        st.stop()
 
-except:
-    st.error("종목코드를 확인하세요.")
+df = fdr.DataReader(code)
+
+st.write(df.tail())
+
+current_price = df['Close'].iloc[-1]
+
+st.metric("현재가", f"{current_price:,}원")
+
+
+
+
+   
+
+  
     user_input = st.text_input("종목명 또는 종목코드 입력", "삼성전자")
 
 listing = fdr.StockListing('KRX')
