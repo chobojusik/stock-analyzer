ddf1ac0941be4993c99f789c9092c820f704d36d
초보주식 <frog88v@gmail.com>
2026-05-18 22:04:37 +0900

    add trading data

--- app.py
+++ app.py
@@ -145,6 +145,19 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig, use_container_width=True)
+# 외국인 기관 수급
+st.subheader("외국인 / 기관 수급")
+
+today = datetime.today()
+start = today - timedelta(days=7)
+
+trading = stock.get_market_trading_value_by_date(
+    start.strftime("%Y%m%d"),
+    today.strftime("%Y%m%d"),
+    code
+)
+
+st.dataframe(trading)
 today = datetime.today()
 start = today - timedelta(days=7)
 
